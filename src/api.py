d95ee775a40b76b19f28a2c5b75f02e9b5fab9f6
James Nicholson <jnicholson@hsph.harvard.edu>
2024-03-26 08:07:13 -0700

    Connect CDF to API

--- src/api.py
+++ src/api.py
@@ -1,6 +1,6 @@
 import logging
 
-from .cdf.cdf import Cdf
+from .cdf.cdf import CDF
 from .consts import (
     CLINICAL_DATA_HEADERS,
     LITERATURE_DATA_HEADERS,
@@ -33,13 +33,13 @@ def invoke_model(prompt):
     return hectre.invoke_model(prompt)
 
 
-def extract_literature_data(paper: Paper, picos: Picos, cdf: Cdf) -> None:
+def extract_literature_data(paper: Paper, picos: Picos, cdf: CDF) -> None:
     '''
     Extract all the literature data such as authors, title, etc.
     Modifies the CDF in-place.
     '''
 
-    paper_id = paper.get_id()
+    lit_data = {}
 
     # First, loop over every literature data header we need in CDF
     for header in LITERATURE_DATA_HEADERS:
@@ -53,17 +53,18 @@ def extract_literature_data(paper: Paper, picos: Picos, cdf: Cdf) -> None:
             result = hectre.query_literature_data(header=header, text=text, text_context=text_context)
             # If we got a non-null result, it means we found it.
             if result:
-                # Set the value in the CDF
-                # TODO
+                # Set the value in lit_data dict
+                lit_data[header] = result
                 break
+    cdf.set_literature_data(lit_data)
+    
 
 
-def extract_clinical_data(paper: Paper, picos: Picos, cdf: Cdf) -> None:
+def extract_clinical_data(paper: Paper, picos: Picos, cdf: CDF) -> None:
     '''
     Extract all the clinical data.
     Modifies the CDF in-place.
     '''
-    # TODO
 
     paper_id = paper.get_id()
     num_pages = paper.get_num_pages()
@@ -140,11 +141,12 @@ def extract_clinical_data(paper: Paper, picos: Picos, cdf: Cdf) -> None:
                         # TODO: We need to somehow conglomerate the data
                         if result:
                             # Set the value in the CDF
-                            # TODO
+                            logger.info("RESULT")
+                            logger.info(result)
                             break
 
 
-def extract_data_from_objects(paper: Paper, picos: Picos) -> Cdf:
+def extract_data_from_objects(paper: Paper, picos: Picos) -> CDF:
     '''
     Main entry function that initiates the extraction process.
     TODO: Finish this function up
@@ -156,16 +158,16 @@ def extract_data_from_objects(paper: Paper, picos: Picos) -> Cdf:
     Returns:
         No output. This function will take a while, so we will store the output elsewhere.
     '''
-    cdf = Cdf()
+    cdf = CDF()
 
     extract_literature_data(paper, picos, cdf)
-
+    # TODO remove
     extract_clinical_data(paper, picos, cdf)
 
     return cdf
 
 
-def extract_data(file_path: str = None, url: str = None, picos_string: str = None) -> Cdf:
+def extract_data(file_path: str = None, url: str = None, picos_string: str = None) -> CDF:
     '''
     Overarching function to turn a file path and a PICOS string into a CDF object.
     '''
--- src/local_test_parse_file_path.py
+++ src/local_test_parse_file_path.py
@@ -1,7 +1,9 @@
 import click
 
 from .api import extract_data
-from .cdf.cdf import Cdf
+from .cdf.cdf import CDF
+import logging
+logger = logging.getLogger(__name__)
 
 
 '''
@@ -14,8 +16,10 @@ This file is used to run a full execution on a PDF file locally.
 @click.argument('picos_string', type=str)
 def extract_with_file_path(file_path: str, picos_string: str):
     # This will fail, since a lot of parts are not implemented yet
-    cdf: Cdf = extract_data(file_path=file_path, picos_string=picos_string)
-    click.echo(str(cdf))
+    cdf: CDF = extract_data(file_path=file_path, picos_string=picos_string)
+    cdf_df = cdf.to_df()
+    #click.echo(cdf_df)
+    cdf_df.to_csv("test_output.csv")
 
 
 if __name__ == '__main__':
