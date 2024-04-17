9936b112e667e8409f16d9688f5081bbe2f5c5ac
James Nicholson <jnicholson@hsph.harvard.edu>
2024-04-17 10:49:02 -0700

    Removed commented code in accuracy.py

--- hectre/metrics/accuracy.py
+++ hectre/metrics/accuracy.py
@@ -13,13 +13,7 @@ from hectre.consts import HEADER_ORDER
 def cdf_accuracy(path_to_pdf: str, picos_string: str, path_to_cdf: str):
     path_to_test_pdf = Path(path_to_pdf)
     path_to_control_cdf = Path(path_to_cdf)
-    # path_to_fake_control_cdf = Path("hectre/tests/test_data/cdfs/fake_305_deBruin_2018.csv")
     test_cdf: Optional[CDF] = extract_data(file_path=path_to_test_pdf.resolve(), picos_string=picos_string)
-    ### FAKE TEST CDF
-    # fake_test_cdf_r1 = CDFData(**{key:"zero" for key in HEADER_ORDER if key != 'DSID'})
-    # test_cdf = CDF()
-    # test_cdf.add_clinical_data(fake_test_cdf_r1)
-    # test_cdf.set_literature_data(fake_test_cdf_r1)
     if not test_cdf:
         raise RuntimeError(f'HECTRE failed to produce a cdf for the PDF located at {path_to_pdf}')
     # Create a DataFrame from the control CDF CSV.
