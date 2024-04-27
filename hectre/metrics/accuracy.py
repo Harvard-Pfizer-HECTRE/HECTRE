e705032416b72e9941a3d5efb0540d15f653fa05
James Nicholson <jnicholson@hsph.harvard.edu>
2024-04-26 23:04:25 -0700

    Max matching working with reporting

--- hectre/metrics/accuracy.py
+++ hectre/metrics/accuracy.py
@@ -3,6 +3,7 @@ from typing import Optional
 from pathlib import Path
 import click
 import pandas as pd
+import numpy as np
 from hectre.api import extract_data
 from hectre.cdf.cdf import CDF
 
@@ -27,9 +28,6 @@ def cdf_accuracy(path_to_pdf: str, picos_string: str, path_to_cdf: str):
     # TODO change back to "test_cdf.compare" and "test_cdf.to_df()" when done testing.
     accuracy = CDF.compare(test_cdf, control_cdf)
     logger.info(f'\nACCURACY OF HECTRE EXTRACTION: {path_to_pdf}:')
-    logger.info(f"\nMatched control rows: {accuracy['num_matched_rows']} of {accuracy['control_clin_data'].shape[0]}")
-    logger.info(f"\nCONTROL PRIMARY KEYS\n{accuracy['control_clin_data'].index}")
-    logger.info(f"\nTEST PRIMARY KEYS\n{accuracy['test_clin_data'].index}")
     lit_acc_pct = accuracy['comp_values_lit'].sum() / accuracy['comp_values_lit'].size
     lit_vals = []
     for col in accuracy['comp_values_lit'].index:
@@ -37,26 +35,25 @@ def cdf_accuracy(path_to_pdf: str, picos_string: str, path_to_cdf: str):
     avg_lit_acc = f'\nAverage string similarity (percent similar): {lit_acc_pct:.2f}\n'
     lit_vals_str = '\n'.join(lit_vals)
     logger.info(f"\nACCURACY OF LITERATURE DATA VALUES (indexed by column name):\n{avg_lit_acc}\n{lit_vals_str}")
-    clin_data_rows = []
-    for i, row in accuracy['comp_rows_clin'].iterrows():
-        clin_data_rows.append(f'\n{i}\n{row.to_string()}')
-    clin_data_rows_str = '\n'.join(clin_data_rows)
-    logger.info(f"\nACCURACY OF CLINICAL DATA ROWS (indexed by control compound primary key):\n{clin_data_rows_str}")
+    num_control_rows = accuracy['control_clin_data'].shape[0]
+    matched_control_rows = num_control_rows - accuracy['row_matches_clin']['Matched Test Row'].isna().sum() 
+    logger.info(f"\nMatched control rows: {matched_control_rows} of {num_control_rows}")
+    logger.info(f"\nCONTROL ROW MATCHES\n{accuracy['row_matches_clin']}")
     clin_data_cols = []
     for col in accuracy['comp_values_clin'].columns:
         pct = (accuracy['comp_values_clin'][col].sum() / accuracy['comp_values_clin'].shape[0])
         clin_data_cols.append(f'{col}: {pct:.2f}')
     clin_data_cols_str = '\n'.join(clin_data_cols)
     logger.info(f"\nACCURACY OF CLINICAL DATA COLUMNS (indexed by column name):\n{clin_data_cols_str}")
-    # Check if there are any matching rows.
-    test_control_clin_join = accuracy['control_clin_data'].merge(accuracy['test_clin_data'], left_index=True, right_index=True)
     # If test and control have at least one matching row, display an example.
-    if test_control_clin_join.shape[0] > 0:
+    if matched_control_rows > 0:
         logger.info('\nROW 1 OF THE MATCHED CDFs: TEST vs. CONTROL')
         # Index of first row in the joined df. Should exist in test and control.
-        matched_compound_key = test_control_clin_join.index[0]
-        test_r1 = pd.concat([accuracy['test_lit_data'], accuracy['test_clin_data'].loc[matched_compound_key]])
-        control_r1 = pd.concat([accuracy['control_lit_data'], accuracy['control_clin_data'].loc[matched_compound_key]])
+        matched_rows = accuracy['row_matches_clin'][accuracy['row_matches_clin']['Matched Test Row'] != np.NaN]
+        matched_control_key = matched_rows.index[0]
+        matched_test_key = matched_rows.loc[matched_control_key, 'Matched Test Row']
+        test_r1 = pd.concat([accuracy['test_lit_data'], accuracy['test_clin_data'].loc[matched_test_key]])
+        control_r1 = pd.concat([accuracy['control_lit_data'], accuracy['control_clin_data'].loc[matched_control_key]])
         df_r1 = pd.DataFrame(data={'Test': test_r1.to_list(), 'Control': control_r1.to_list()}, index=test_r1.index, columns=['Test', 'Control'])
         pd.set_option('display.max_rows', df_r1.shape[0])
         logger.info(df_r1)
